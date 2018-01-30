318e392fc86a66e5b9306aa1290fd5bc93072db6
Michael <mihail.tchernov@yandex.ru>
2018-01-30 10:32:21 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='SeisPars',
+      version='0.0.1',
+      packages=find_packages(),
+      description='Package for processing of microseismic data',
+      author='Michael Chernov',
+      author_email='mikkoartic@gmail.com',
+      license='MIT',
+      zip=False)
