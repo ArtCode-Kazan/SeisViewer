02f0bda2215e049c02e8036c825648a9d1e8cce8
Michael <mihail.tchernov@yandex.ru>
2018-03-30 08:49:38 +0300

    version 0.1.1 Исправлен GUI-интерфейс - расширен частотный диапазон для анализа и отрисовки спектров и спектрограмм

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='SeisRevise',
-      version='0.1.0',
+      version='0.1.1',
       packages=find_packages(),
       description='Package for processing of microseismic data',
       author='Michael Chernov',
