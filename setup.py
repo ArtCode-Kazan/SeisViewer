a6da43f7b5e74f263bf2cf9a266f96f4016aa09d
Michael <mihail.tchernov@yandex.ru>
2018-07-19 13:06:29 +0300

    version 0.2.2 Добавлена опция выгрузки участка сигнала для строящейся спектрограммы. Изменена структура ORM - добавлен тип данных (GRP | MSI). Убран тип экспорта в расчете спектрограмм. Зависимости: SeisPars 0.1.2, SeisCore 0.0.34sm2ref

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='SeisRevise',
-      version='0.2.2beta',
+      version='0.2.2',
       packages=find_packages(),
       description='Package for processing of microseismic data',
       author='Michael Chernov',
