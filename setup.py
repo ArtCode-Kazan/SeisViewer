55813633c6aa9adf0387e3da01bb3e8f391f7046
Michael <mihail.tchernov@yandex.ru>
2018-08-02 09:22:03 +0300

    version 0.2.7 Пакет переведен в основную ветку dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='SeisRevise',
-      version='0.2.7ref',
+      version='0.2.7',
       packages=find_packages(),
       description='Package for processing of microseismic data',
       author='Michael Chernov',
