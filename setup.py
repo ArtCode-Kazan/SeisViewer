5946c91f37a3bc3d54486bd7cbe7007f8a797ab5
Michael <mihail.tchernov@yandex.ru>
2019-12-06 14:55:48 +0300

    version 1.0.4 Add new option: stitching binary files (for Baikal7) Requires: SeisCore 0.3.7

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(name='SeisRevise',
       license='MIT',
       include_package_data=True,
       zip=False,
-      install_requires=['SeisCore'],
-      dependency_links=['http://github.com/MikkoArik/SeisCore/tarball/dev#egg=SeisCore'])
+      install_requires=['SeisCore @ '
+                        'git+https://github.com/MikkoArtik/SeisCore.git@dev#egg=SeisCore'])
